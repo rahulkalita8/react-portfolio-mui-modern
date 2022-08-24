81652b916678d25b10b07c50dbf0e5c7c7f894c5
Rahul Kalita <rahulkalita8@gmail.com>
2022-08-24 17:53:52 -0400

    Adding navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+
+import "./App.css";
+import theme from "./assets/theme/theme";
+import HideAppBar from "./components/NavigationBar";
+import DrawerAppBar from "./components/DrawerAppBar";
+import Typography from "@mui/material/Typography";
+import { Box, Container, Toolbar } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <DrawerAppBar />
+        <Toolbar />
+        <Typography>
+          {[...new Array(30)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+            )
+            .join("\n")}
+        </Typography>
+      </Box>
+    </ThemeProvider>
   );
 }
 
